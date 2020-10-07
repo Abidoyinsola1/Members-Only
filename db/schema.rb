28aefc113213e82c3023b54d82df120aa8278e5a
haywhizzz <haywhizzz@gmail.com>
2020-10-07 16:47:08 +0100

    add migration for user_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_07_151130) do
+ActiveRecord::Schema.define(version: 2020_10_07_154436) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
