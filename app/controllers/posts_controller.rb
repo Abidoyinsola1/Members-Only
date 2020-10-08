b2a52e0e91264bffafef32bdee1baa2be5899b1a
Abiola Doyinsola <ifeobayemi@gmail.com>
2020-10-08 09:51:08 +0100

    Fix bugs, update index and pages. Add authetication

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   # GET /posts
   # GET /posts.json
@@ -69,6 +70,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.fetch(:post, {})
+      params.require(:post).permit(:title, :body, :user_id)
     end
 end
